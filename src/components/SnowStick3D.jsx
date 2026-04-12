3aa80fc91ffb8341256e3cee911ffa4cd10579e4
Gunvald <72520417+fille11@users.noreply.github.com>
2026-04-12 13:36:21 +0200

    Update SnowStick3D.jsx

--- src/components/SnowStick3D.jsx
+++ src/components/SnowStick3D.jsx
@@ -2,16 +2,18 @@ import { Canvas } from "@react-three/fiber";
 import { useGLTF, OrbitControls } from "@react-three/drei";
 
 function Model() {
-  const { scene } = useGLTF("/snowstick.glb");
+  const { scene } = useGLTF(
+    "https://xonbkazvfxllffjbqfdm.supabase.co/storage/v1/object/public/models/snowstick1.glb"
+  );
 
-  return <primitive object={scene} scale={2} />;
+  return <primitive object={scene} scale={2} position={[0, -1, 0]} />;
 }
 
 export default function SnowStick3D() {
   return (
     <Canvas camera={{ position: [0, 1, 5] }}>
-      <ambientLight intensity={1} />
-      <directionalLight position={[2, 2, 2]} />
+      <ambientLight intensity={1.5} />
+      <directionalLight position={[2, 2, 2]} intensity={2} />
 
       <Model />
 
