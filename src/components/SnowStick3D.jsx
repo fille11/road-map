69aaf0cbce47e5d5941c3baf1d3e4dd9eb26c473
Gunvald <72520417+fille11@users.noreply.github.com>
2026-04-12 13:23:07 +0200

    Update SnowStick3D.jsx

--- src/components/SnowStick3D.jsx
+++ src/components/SnowStick3D.jsx
@@ -3,21 +3,19 @@ import { useGLTF, OrbitControls } from "@react-three/drei";
 
 function Model() {
   const { scene } = useGLTF("/snowstick.glb");
+
   return <primitive object={scene} scale={2} />;
 }
 
 export default function SnowStick3D() {
   return (
-    <div style={{ width: "100%", height: "400px" }}>
-      <Canvas camera={{ position: [0, 0, 5] }}>
-        <ambientLight intensity={1} />
-        <directionalLight position={[2, 2, 2]} />
+    <Canvas camera={{ position: [0, 1, 5] }}>
+      <ambientLight intensity={1} />
+      <directionalLight position={[2, 2, 2]} />
 
-        <Model />
+      <Model />
 
-        {/* valfri rotation med mus */}
-        <OrbitControls enableZoom={false} />
-      </Canvas>
-    </div>
+      <OrbitControls />
+    </Canvas>
   );
 }
